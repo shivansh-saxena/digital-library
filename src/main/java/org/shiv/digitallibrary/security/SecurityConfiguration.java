832acfc8f4842d7cd54cc764c3e39975bdc95b78
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-03-19 20:59:50 +0530

    disabling security using SecurityConfiguration

---
+++ src/main/java/org/shiv/digitallibrary/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package org.shiv.digitallibrary.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                {
+//                    auth.requestMatchers("").permitAll();
+                    auth.anyRequest().permitAll();
+                }
+        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
